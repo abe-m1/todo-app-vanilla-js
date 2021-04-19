e11abe54fdeff4ca64304ee4f6a6722af5cbad91
abe menashy <abestbet1@gmail.com>
2021-04-19 11:30:08 -0700

    list item remove and complete

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ const input = document.getElementById('new-todo');
 const todoList = document.getElementById('todo-list');
 const todoItems = document.getElementsByClassName('todo-item');
 const footer = document.querySelector('.footer');
-todoList.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('ck', e.target.input, todoItems);
-});
+const clearComplete = document.getElementById('clear-complete');
+const itemsLeft = document.getElementById('items-left');
+
+// todoList.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   console.log('ck', e.target.input, todoItems);
+// });
+
+function removeItem(e) {
+  console.log('ck', e, todoItems);
+}
 
 input.addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
@@ -22,6 +29,18 @@ input.addEventListener('keyup', function (event) {
     img.setAttribute('alt', 'close');
     cancel.appendChild(img);
 
+    cancel.addEventListener('click', function () {
+      var li = this.parentNode;
+      li.remove();
+      itemsLeft.innerText = `${todoItems.length} items left`;
+    });
+
+    circle.addEventListener('click', function () {
+      circle.classList.toggle('task-complete');
+      console.log(circle.parentElement.parentElement);
+      circle.parentElement.classList.toggle('line-through');
+    });
+
     let todoText = document.createTextNode(input.value);
     circle.className = 'round';
 
@@ -34,5 +53,21 @@ input.addEventListener('keyup', function (event) {
     console.log(input.value);
 
     footer.style.display = 'flex';
+    input.value = '';
   }
+  itemsLeft.innerText = `${todoItems.length} items left`;
+});
+
+clearComplete.addEventListener('click', function () {
+  console.log(todoItems);
+  // todoItems.forEach((item) => console.log('item', item));
+  for (let item of todoItems) {
+    console.log(item.firstChild);
+    if (item.firstChild.classList.contains('task-complete')) {
+      console.log('complete');
+      // var li = item.parentNode;
+      item.remove();
+    }
+  }
+  itemsLeft.innerText = `${todoItems.length} items left`;
 });
