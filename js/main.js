5a706fea91faaa416f00e837096d97f3b00472ca
abe menashy <abestbet1@gmail.com>
2021-04-21 07:42:47 -0700

    drag and drop

--- js/main.js
+++ js/main.js
@@ -9,12 +9,35 @@ const allItems = document.getElementById('all-items');
 const activeItems = document.getElementById('active-items');
 const completedItems = document.getElementById('completed-items');
 
+const toggleButton = document.querySelector('.toggle-button');
+
 allItems.style.color = 'red';
 
-// todoList.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   console.log('ck', e.target.input, todoItems);
-// });
+const setDraggedOver = (e) => {
+  e.preventDefault();
+  draggedOver = Number.isNaN(parseInt(e.target.innerText))
+    ? e.target.innerText
+    : parseInt(e.target.innerText);
+};
+
+const setDragging = (e) => {
+  dragging = Number.isNaN(parseInt(e.target.innerText))
+    ? e.target.innerText
+    : parseInt(e.target.innerText);
+};
+
+const reorder = (e) => {
+  var index1 = Array(...todoItems).findIndex(
+    (item) => item.innerText === dragging
+  );
+  var index2 = Array(...todoItems).findIndex(
+    (item) => item.innerText === draggedOver
+  );
+
+  let a = Array(...todoItems).splice(index1, 1);
+  todoItems[index1].parentNode.removeChild(todoItems[index1]);
+  todoList.insertBefore(a[0], todoItems[index2]);
+};
 
 function removeItem(e) {
   console.log('ck', e, todoItems);
@@ -54,6 +77,11 @@ input.addEventListener('keyup', function (event) {
     newLi.appendChild(todoText);
     newLi.appendChild(cancel);
 
+    newLi.draggable = true;
+    newLi.addEventListener('drag', setDragging);
+    newLi.addEventListener('dragover', setDraggedOver);
+    newLi.addEventListener('drop', reorder);
+
     todoList.appendChild(newLi);
 
     console.log(input.value);
@@ -66,12 +94,10 @@ input.addEventListener('keyup', function (event) {
 
 clearComplete.addEventListener('click', function () {
   console.log(todoItems);
-  // todoItems.forEach((item) => console.log('item', item));
   for (let item of todoItems) {
     console.log(item.firstChild);
     if (item.firstChild.classList.contains('task-complete')) {
       console.log('complete');
-      // var li = item.parentNode;
       item.remove();
     }
   }
@@ -115,3 +141,8 @@ function filterItems(status = '') {
 activeItems.addEventListener('click', () => filterItems('active'));
 completedItems.addEventListener('click', () => filterItems('completed'));
 allItems.addEventListener('click', () => filterItems());
+
+toggleButton.addEventListener('click', () => {
+  console.log('toggle');
+  toggleButton.classList.toggle('toggle-light');
+});
